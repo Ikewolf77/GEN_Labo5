8ae9951e41e754542134f4bf5ed531c1cc71e820
Simon <simon.mattei@heig-vd.ch>
2020-06-04 10:24:05 +0200

    bad smell : temporary field ; refactor : removed temp field and using this

--- Customer_Rental/Rental.cpp
+++ Customer_Rental/Rental.cpp
@@ -11,33 +11,32 @@ string Rental::getFigures(double &totalAmount, int &frequentRenterPoints) const 
 
     ostringstream result;
     double thisAmount = 0;
-    Rental each = *this;
 
     // determine amounts for each line
-    switch ( each.getMovie().getPriceCode() ) {
+    switch ( getMovie().getPriceCode() ) {
         case Movie::REGULAR:
             thisAmount += 2;
-            if ( each.getDaysRented() > 2 )
-                thisAmount += ( each.getDaysRented() - 2 ) * 1.5 ;
+            if ( getDaysRented() > 2 )
+                thisAmount += ( getDaysRented() - 2 ) * 1.5 ;
             break;
         case Movie::NEW_RELEASE:
-            thisAmount += each.getDaysRented() * 3;
+            thisAmount += getDaysRented() * 3;
             break;
         case Movie::CHILDRENS:
             thisAmount += 1.5;
-            if ( each.getDaysRented() > 3 )
-                thisAmount += ( each.getDaysRented() - 3 ) * 1.5;
+            if ( getDaysRented() > 3 )
+                thisAmount += ( getDaysRented() - 3 ) * 1.5;
             break;
     }
 
     // add frequent renter points
     frequentRenterPoints++;
     // add bonus for a two day new release rental
-    if ( ( each.getMovie().getPriceCode() == Movie::NEW_RELEASE )
-         && each.getDaysRented() > 1 ) frequentRenterPoints++;
+    if ( ( getMovie().getPriceCode() == Movie::NEW_RELEASE )
+         && getDaysRented() > 1 ) frequentRenterPoints++;
 
     // show figures for this rental
-    result << "\t" << each.getMovie().getTitle() << "\t"
+    result << "\t" << getMovie().getTitle() << "\t"
            << thisAmount << "\n";
     totalAmount += thisAmount;
 
